009c65d0a81d69dec710ec0eadc43230ad8fa11e
dbolivar25 <danielbolivarflorez@gmail.com>
2024-12-29 01:24:18 -0500

    update errors

--- src/error.rs
+++ src/error.rs
@@ -1,33 +1,4 @@
-use std::fmt::Display;
-
 use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub struct TokenizerError {}
-
-impl Display for TokenizerError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "TODO")
-    }
-}
-
-#[derive(Error, Debug)]
-pub struct ParserError {}
-
-impl Display for ParserError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "TODO")
-    }
-}
-
-#[derive(Error, Debug)]
-pub struct RuntimeError {}
-
-impl Display for RuntimeError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "TODO")
-    }
-}
 
 #[derive(Error, Debug)]
 pub enum Error {
@@ -35,12 +6,12 @@ pub enum Error {
     IO(#[from] std::io::Error),
     #[error("parse float error: {0}")]
     ParseFloat(#[from] std::num::ParseFloatError),
-    #[error("tokenizer error: {0}")]
-    Tokenizer(TokenizerError),
-    #[error("parser error: {0:?}")]
-    Parser(Vec<ParserError>),
-    #[error("runtime error: {0:?}")]
-    Runtime(Vec<RuntimeError>),
+    #[error("tokenizer error: {message}")]
+    Tokenizer { message: String },
+    #[error("parser error: {message}")]
+    Parser { message: String },
+    #[error("runtime error: {message}")]
+    Runtime { message: String },
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
